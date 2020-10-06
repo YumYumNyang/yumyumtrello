10c0b7de5e862ab352898d31569484804aa73318
YumYumNyang <khw121699@gmail.com>
2020-10-06 17:10:40 +0900

    Updates

---
+++ precache-manifest.e643491dfadb428568e1139ad949933e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37b45e24ef7221ab40e9f32322636b03",
+    "url": "/yumyumtrello/index.html"
+  },
+  {
+    "revision": "846530df926c4f2ca110",
+    "url": "/yumyumtrello/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "628adef14172bb853672",
+    "url": "/yumyumtrello/static/js/2.d623feb1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/yumyumtrello/static/js/2.d623feb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "846530df926c4f2ca110",
+    "url": "/yumyumtrello/static/js/main.1c75b85e.chunk.js"
+  },
+  {
+    "revision": "3b467a11fd0e7af6257e",
+    "url": "/yumyumtrello/static/js/runtime-main.72642634.js"
+  }
+]);
